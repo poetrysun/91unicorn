3c8bd1b438b2d5a77fcd92f233bdc9435b37ca9b
poetrysun <108463047+poetrysun@users.noreply.github.com>
2023-01-14 14:34:08 +0800

    Add files via upload

---
+++ env.js
@@ -0,0 +1,27 @@
+window.settings = {
+  // 站点标题
+  title: '独角兽',
+  // 站点描述
+  description: '💖让你的网络拥有魔法',
+  // API
+  host: 'https://api.91unicorn.cloud',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'light',
+    color: 'darkblue'
+  },
+  // 背景
+  background_url: 'https://files.catbox.moe/35ksmp.jpg',
+  // crisp
+  crisp_id: '',
+  i18n: [
+    'zh-CN',
+    'en-US',
+    'ja-JP',
+    'vi-VN',
+    'ko-KR',
+    'zh-TW',
+    'fa-IR'
+  ]
+}
