0ae98d580ab632d1d961cabc6b09917d8e4d17ce
poetrysun <108463047+poetrysun@users.noreply.github.com>
2023-01-14 15:15:33 +0800

    Add files via upload

---
+++ ania/env.js
@@ -0,0 +1,17 @@
+window.settings = {
+  // 站点标题
+  title: '独角兽管理页',
+  // API
+  host: 'https://api.91unicorn.cloud',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'light',
+    color: 'darkblue'
+  },
+  // 背景
+  background_url: 'https://files.catbox.moe/35ksmp.jpg',
+  logo: '',
+  // 需与V2Board设置中的后台路径一致
+  secure_path: 'ania'
+}
